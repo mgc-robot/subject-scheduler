32d691f453b28f6937ade33df2d731e933ade39c
Mark Gerald Cabatingan <markgerald.nst@gmail.com>
2016-07-01 17:16:51 +0800

    set up laravel dingo api

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Zizaco\Entrust\EntrustServiceProvider::class,
+        Dingo\Api\Provider\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
