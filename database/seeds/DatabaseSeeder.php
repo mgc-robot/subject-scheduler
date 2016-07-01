43a6a35bc3744ae289d497dd5641c7e79c1a5484
Mark Gerald Cabatingan <markgerald.nst@gmail.com>
2016-07-01 15:53:17 +0800

    add database seeder usint entrust trait

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,11 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
         $this->call(SubjectsTableSeeder::class);
         $this->call(RoomsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-//        $this->call(SchedulesTableSeeder::class);
 //        $this->call(SchedulesTableSeeder::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,40 +17,102 @@ class UsersTableSeeder extends Seeder
 
         $dataRoot = [
             'id'         => 1,
-            'email'      => 'footless@demo.com',
-            'username'   => 'footless',
+            'email'      => 'mark@demo.com',
             'password'   => Hash::make('password'),
-            'first_name' => 'Footless',
-            'last_name'  => 'Hero',
+            'first_name' => 'Mark Gerald',
+            'last_name'  => 'Cabatingan',
             'gender'     => 'm'
         ];
 
         $userRoot = new User($dataRoot);
         $userRoot->save();
 
-//        $userRoot->attachRole(1);
-        DB::table('users')->insert([
-            [
+        $userRoot->attachRole(1);
 
-            ],
+        $dataAdmin = [
+            'id'         => 2,
+            'email'      => 'footless@demo.com',
+            'password'   => Hash::make('password'),
+            'first_name' => 'Footless',
+            'last_name'  => 'Hero',
+            'gender'     => 'm'
+        ];
+
+        $userRoot = new User($dataAdmin);
+        $userRoot->save();
+
+        $userRoot->attachRole(2);
+        $dataInstructor =
             [
-                'id'         => 2,
-                'email'      => 'john_doe@demo.com',
-                'username'   => 'john_doe',
-                'password'   => Hash::make('password'),
-                'first_name' => 'John',
-                'last_name'  => 'Doe',
-                'gender'     => 'm'
-            ],
-            [
-                'id'         => 3,
-                'email'      => 'jon@sample.com',
-                'username'   => 'jon',
-                'password'   => Hash::make('password'),
-                'first_name' => 'Jon',
-                'last_name'  => 'Snow',
-                'gender'     => 'm'
-            ]
-        ]);
+                [
+                    'id'         => 3,
+                    'email'      => 'test_instructor1@gmail.com',
+                    'password'   => Hash::make('password'),
+                    'first_name' => 'Jon 1',
+                    'last_name'  => 'Doe',
+                    'gender'     => 'm'
+                ],
+                [
+                    'id'         => 4,
+                    'email'      => 'test_instructor2@gmail.com',
+                    'password'   => Hash::make('password'),
+                    'first_name' => 'Jon 2',
+                    'last_name'  => 'Snow',
+                    'gender'     => 'm'
+                ],
+                [
+                    'id'         => 5,
+                    'email'      => 'test_instructor3@gmail.com',
+                    'password'   => Hash::make('password'),
+                    'first_name' => 'Jon 3',
+                    'last_name'  => 'Snow',
+                    'gender'     => 'm'
+                ],
+                [
+                    'id'         => 6,
+                    'email'      => 'test_instructor4@gmail.com',
+                    'password'   => Hash::make('password'),
+                    'first_name' => 'Jon 4',
+                    'last_name'  => 'Snow',
+                    'gender'     => 'm'
+                ],
+                [
+                    'id'         => 7,
+                    'email'      => 'test_instructor5@gmail.com',
+                    'password'   => Hash::make('password'),
+                    'first_name' => 'Jon 5',
+                    'last_name'  => 'Snow',
+                    'gender'     => 'm'
+                ],
+                [
+                    'id'         => 8,
+                    'email'      => 'test_instructor6@gmail.com',
+                    'password'   => Hash::make('password'),
+                    'first_name' => 'Jon 6',
+                    'last_name'  => 'Snow',
+                    'gender'     => 'm'
+                ]
+            ];
+
+        foreach ($dataInstructor as $instructor) {
+            $userRoot = new User($instructor);
+            $userRoot->save();
+
+            $userRoot->attachRole(3);
+        }
+
+        $dataStudent = [
+            'id'         => 8,
+            'email'      => 'test_student@gmail.com',
+            'password'   => Hash::make('password'),
+            'first_name' => 'Jon student',
+            'last_name'  => 'Snow',
+            'gender'     => 'm'
+        ];
+
+        $userRoot = new User($dataStudent);
+        $userRoot->save();
+
+        $userRoot->attachRole(4);
     }
 }
