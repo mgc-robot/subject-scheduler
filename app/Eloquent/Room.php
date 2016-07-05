1cb751ffff398171b3437360d37f10505da45a2d
Mark Gerald Cabatingan <markgerald.nst@gmail.com>
2016-07-05 16:22:17 +0800

    add relationship to eloquent models

--- app/Eloquent/Room.php
+++ app/Eloquent/Room.php
@@ -9,4 +9,11 @@ class Room extends Model
     protected $table = 'rooms';
     protected $fillable = ['room_no', 'room_type'];
     protected $dates = ['created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function schedules(){
+        return $this->hasMany('App\Eloquent\Schedule');
+    }
 }
--- app/Eloquent/User.php
+++ app/Eloquent/User.php
@@ -26,4 +26,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function schedules(){
+        return $this->hasMany('App\Eloquent\Schedule');
+    }
 }
